425e18297e4925819690a8c4d0fac218c036760a
ZePaiva <zmppaiva99@gmail.com>
2019-10-17 00:42:07 +0100

    missing something, can't find out what right now, will take a look tomorrorw morning

--- Project/player.py
+++ Project/player.py
@@ -1,57 +1,57 @@
 import json
 
 class Player:
-	def __init__(self, hand=[], player_id=''):
-		self.hand = hand
-		self.id = player_id
-		self.order = None
-		self.points = 0
-		self.used_cards = []
+    def __init__(self, hand=[], player_id=''):
+        self.hand = hand
+        self.id = player_id
+        self.order = None
+        self.points = 0
+        self.used_cards = []
 
-	def update_hand(self, hand):
-		self.hand = hand
+    def update_hand(self, hand):
+        self.hand = hand
 
-	def update_id(self, player_id):
-		self.id = player_id
+    def update_id(self, player_id):
+        self.id = player_id
 
-	def update_order(self, order):
-		self.order = order
+    def update_order(self, order):
+        self.order = order
 
-	def update_points(self, points):
-		self.points += points
+    def update_points(self, points):
+        self.points += points
 
-	def update_used_cards(self, cards):
-		self.used_cards.extend(cards)
+    def update_used_cards(self, cards):
+        self.used_cards.extend(cards)
 
-	def communicate_has_2C(self, s):
-		payload = {
-			"operation":"player@has_two_of_clubs",
-			"player_id": self.id,
-			"has_2C": "2C" in self.hand		
-		}
-		s.send(json.dumps(payload).encode())
+    def communicate_has_2C(self, s):
+        payload = {
+            "operation":"player@has_two_of_clubs",
+            "player_id": self.id,
+            "has_2C": "2C" in self.hand     
+        }
+        s.send(json.dumps(payload).encode())
 
-	def communicate_is_ready(self, s):
-		payload = {
-			"operation":"player@is_ready",
-			"player_id": self.id,
-			"order": self.order,
-			"ready": True
-		}
-		s.send(json.dumps(payload).encode())
+    def communicate_is_ready(self, s):
+        payload = {
+            "operation":"player@is_ready",
+            "player_id": self.id,
+            "order": self.order,
+            "ready": True
+        }
+        s.send(json.dumps(payload).encode())
 
-	def play(self, s):
-		card = ''
-		print('Your hand: ' + str(self.hand))
-		while card not in self.hand:
-			card = input('Play a card from your hand: ')
-		self.hand.remove(card)
-		payload = {
-			"operation":"player@play",
-			"player_id": self.id,
-			"order": self.order,
-			"card" : str(card)	
-		}
-		s.send(json.dumps(payload).encode())
+    def play(self, s):
+        card = ''
+        print('Your hand: ' + str(self.hand))
+        while card not in self.hand:
+            card = input('Play a card from your hand: ')
+        self.hand.remove(card)
+        payload = {
+            "operation":"player@play",
+            "player_id": self.id,
+            "order": self.order,
+            "card" : str(card)  
+        }
+        s.send(json.dumps(payload).encode())
 
 
--- Project/server.py
+++ Project/server.py
@@ -7,7 +7,7 @@ import json
 import traceback
 
 
-host = '127.0.0.1'
+host = '0.0.0.0'
 port = 8080
 
 clients = []
@@ -15,14 +15,14 @@ clients = []
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-	s.bind((host,port))
+    s.bind((host,port))
+    print('socket binding to ' + host + ':' + str(port))
 except Exception as e:
-	print(e)
-	sys.exit(1)
+    print(e)
+    sys.exit(1)
 
 s.listen(4) # listens to up to 4 clients
 print('Waiting for 4 clients')
-
 
 # game related
 croup = Croupier(deck=cards)
@@ -32,80 +32,89 @@ current_suit = ''
 
 
 def clientthread():
-	global clients
-	global croup
-	global current_player_idx
-	global cards_on_table
-	global current_suit
-	conn, addr = clients[-1]
-	identification = addr[0] + ":" + str(addr[1])
-	while 1:
-		data = conn.recv(1024)
-		if not data:
-			break
-		try:
-			payload = json.loads(data.decode())
-			operation = payload['operation']
-			print(payload)
-			if operation == "player@has_two_of_clubs":
-				if payload["has_2C"]:
-					croup.give_order((conn, addr))
-					current_player_idx = 0
-			elif operation == "player@is_ready":
-				print('Current player idx: ', current_player_idx)
-				if payload["order"] == current_player_idx:
-					croup.demand_play_card(current_player_idx)
-			elif operation == "player@play":
-				if payload["order"] == current_player_idx:
-					if current_player_idx == 0:
-						current_suit = payload["card"][-1]
+    global clients
+    global croup
+    global current_player_idx
+    global cards_on_table
+    global current_suit
+    conn, addr = clients[-1]
+    identification = addr[0] + ":" + str(addr[1])
+    while 1:
+        data = conn.recv(1024)
+        if not data:
+            break
+        try:
+            payload = json.loads(data.decode())
+            operation = payload['operation']
+            print(payload)
+            if operation == "player@has_two_of_clubs":
+                if payload["has_2C"]:
+                    croup.give_order((conn, addr))
+                    current_player_idx = 0
+            elif operation == "player@is_ready":
+                print('Current player idx: ', current_player_idx)
+                if payload["order"] == current_player_idx:
+                    croup.demand_play_card(current_player_idx)
+            elif operation == "player@play":
+                if croup.round==0 and current_player_idx==0 and payload['card']!='2C':
+                    croup.demand_play_card(current_player_idx, 'must start with that play')
+                elif not croup.heart_brake and current_player_idx==0 and 'H' in payload['card']:
+                    croup.demand_play_card(current_player_idx, 'hearts not broken')
+                elif payload["order"] == current_player_idx:
+                    if current_player_idx == 0:
+                        current_suit = payload["card"][-1]
 
-					cards_on_table.append((payload["order"], payload["card"]))
-					print(cards_on_table)
+                    croup.round+=1
+                    if 'H' in payload['card']:
+                        croup.heart_brake=True
+                    cards_on_table.append((payload["order"], payload["card"]))
+                    print(cards_on_table)
 
-					if current_player_idx == len(clients)-1:
-						# get the loser of the table
-						cards_for_loser = [c[1] for c in cards_on_table.copy()]
-						# from hearts
-						hc = get_higher_card(cards_for_loser, current_suit)
-						loser_idx = 0
-						for o,c in cards_on_table:
-							if c == hc:
-								loser_idx = o
-								break
+                    if current_player_idx == len(clients)-1:
+                        # get the loser of the table
+                        cards_for_loser = [c[1] for c in cards_on_table.copy()]
+                        # from hearts
+                        hc = get_higher_card(cards_for_loser, current_suit)
+                        loser_idx = 0
+                        for o,c in cards_on_table:
+                            if c == hc:
+                                loser_idx = o
+                                break
 
-						current_suit = ''
-						current_player_idx = 0
-						cards_on_table = []
+                        current_suit = ''
+                        current_player_idx = 0
+                        cards_on_table = []
 
-						croup.give_cards_to_loser(loser_idx, cards_for_loser)
+                        croup.give_cards_to_loser(loser_idx, cards_for_loser)
 
-					else:
-						current_player_idx = (current_player_idx + 1) % 4
-						croup.demand_play_card(current_player_idx)
+                    else:
+                        current_player_idx = (current_player_idx + 1) % 4
+                        croup.share_table(cards_on_table)
+                        croup.demand_play_card(current_player_idx)
 
-		except Exception as e:
-			print(e)
-			traceback.print_exc()
-			print('Not json')
-			print(data.decode())
-		
-	conn.close()
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+            print('Not json')
+            print(data.decode())
+        
+    conn.close()
 
 while 1:
-	try:
-		conn, addr = s.accept()
-		clients.append((conn, addr))
-		print('Connected with ' + addr[0] + ":" + str(addr[1]))
-		start_new_thread(clientthread, ())
-		if len(clients)==4:
-			croup.update_players(clients)
-			croup.give_cards()
+    try:
+        conn, addr = s.accept()
+        clients.append((conn, addr))
+        print('Connected with ' + addr[0] + ":" + str(addr[1]))
+        croup.missing_players(len(clients), clients)
+        start_new_thread(clientthread, ())
+        if len(clients)==4:
+            croup.update_players(clients)
+            croup.give_cards()
 
-	except Exception as e:
-		print(e)
-		s.close()
-		print('Socket closed')
-		print('Exiting')
-		sys.exit(1)
+    except Exception as e:
+        print(e)
+        s.close()
+        print('Socket closed')
+        print('Exiting')
+        sys.exit(1)
 
